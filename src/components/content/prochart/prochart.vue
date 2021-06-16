402e1443762a513736d11e9be0704ffa1e7cea53
Pro-pro-cloud <Pro-pro-cloud>
2021-06-16 18:49:10 +0800

    折线图切换完成

---
+++ src/components/content/prochart/prochart.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="mychart" ref="mychart">
+        prochart
+    </div>
+</template>
+
+<script>
+export default {
+    name : 'prochart',
+    data () {
+        return {
+            
+        }
+    },
+    props : {
+        lineValue :{
+            type : Array,
+            default: []
+        }
+    },
+    watch : {
+        lineValue : {
+            handler(){
+                this.drawLine()
+            },
+            deep:true
+        }
+    },
+    mounted () {
+        this.drawLine()
+    },
+    methods : {
+        drawLine () {
+            let mychart = this.$echarts.init(this.$refs.mychart)
+            let option = {
+                xAxis: {
+                    type: 'category',
+                    data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+                },
+                yAxis: {
+                    type: 'value'
+                },
+                series: [{
+                    data: this.lineValue,
+                    type: 'line',
+                    smooth: true
+                }]
+            }
+        option && mychart.setOption(option)            
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .mychart {
+        height: 600px;
+        width: 600px;
+        border: 1px black solid;
+    }
+</style>
--- src/views/province/mapVue/proMap.vue
+++ src/views/province/mapVue/proMap.vue
@@ -2,26 +2,37 @@
     <div>
         <main-agg-data :general-data="genData"></main-agg-data>
         <div>proMap</div>
+        <div class="btn">
+            <div class="btn-item" @click="btnClick(index)" v-for="(item,index) in btnTittle" :key="index">
+            <button>{{item}}</button>
+            </div>
+        </div>
+        <prochart :lineValue='chartData'></prochart>
     </div>
 </template>
 
 <script>
 
 import MainAggData from '../../../components/content/mainAggData/MainAggData.vue'
+import Prochart from '../../../components/content/prochart/prochart.vue'
 
 export default {
     name: 'proMap',
     data () {
         return {
-            province : null
+            province : null,
+            btnTittle : ['当前确诊','累计确诊'] ,
+            chartData : [820, 932, 901, 934, 1290, 1330, 1320],
+            cnum : [820, 932, 901, 934, 1290, 1330, 1320],
+            addnum : [222,555,444,680,20,1500,2100]
         }
     },
     components:{
-        MainAggData
+        MainAggData,
+        Prochart
     },
     created () {
         this.province = this.$route.query.proname
-        console.log(this.province);
     },
     computed:{
       genData(){
@@ -33,10 +44,32 @@ export default {
           cumCur:{data:16597,add:1598}
         }
       }
+    },
+    methods : {
+        btnClick(index){
+            if(index === 0){
+                this.chartData = this.cnum
+            } else if (index === 1) {
+                this.chartData = this.addnum
+            } else {
+                this.chartData = this.cnum
+            }
+        }
     }
 }
 </script>
 
 <style scoped>
-
+.btn{
+    display: flex;
+    width: 1160px;
+    height: 49px;
+}
+.btn .btn-item{
+    flex:1
+}
+.btn .btn-item button{
+    width: 100%;
+    height: 49px;
+}
 </style>
